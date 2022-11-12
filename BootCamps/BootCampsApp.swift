9eb06cc6f7fe89174d82c78b0574cff1894ba2d3
Layton Dickerson <laytondickersoniii@gmail.com>
2022-11-12 12:12:07 -0800

    Initial Commit

---
+++ BootCamps/BootCampsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BootCampsApp.swift
+//  BootCamps
+//
+//  Created by Layton Dickerson III on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BootCampsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
